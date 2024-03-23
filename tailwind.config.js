d8f4b606c39be8da657796fac8c2c27b53e5da78
Lees20 <149702804+Lees20@users.noreply.github.com>
2024-03-23 14:37:52 +0200

    Init app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
