4233d4685939697445fe0d4211848ad1b5677c82
Lees20 <149702804+Lees20@users.noreply.github.com>
2024-03-23 12:57:28 +0200

    Init app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    FooterComponent,
-    AppComponent,
-    HeaderComponent
+   AppComponent
   ],
   imports: [
     BrowserModule,
